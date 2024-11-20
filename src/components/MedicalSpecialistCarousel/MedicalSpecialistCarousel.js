fed326b6638d71f4f4f7e3ac6b97aeaceb62a08f
unknown <prachipanwar397@gmail.com>
2024-11-20 23:15:50 +0530

    added medical specialist carousel

--- src/components/MedicalSpecialistCarousel/MedicalSpecialistCarousel.js
+++ src/components/MedicalSpecialistCarousel/MedicalSpecialistCarousel.js
@@ -5,17 +5,25 @@ import "swiper/css"; // Import Swiper styles
 import "swiper/css/navigation"; // Import Navigation styles
 import "swiper/css/pagination"; // Import Pagination styles
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import CarouselImg1 from "../../Assets/carouselmg1.png";
-import CarouselImg2 from "../../Assets/carouselImg2.png";
+import doctor1 from "../../Assets/doctor1.png";
+import doctor2 from "../../Assets/doctor2.png";
+import doctor3 from "../../Assets/doctor3.png";
+import doctor4 from "../../Assets/doctor4.png";
+import doctor5 from "../../Assets/doctor5.png";
 
 const MedicalSpecialistCarousel = () => {
-  const images = [CarouselImg1, CarouselImg2, CarouselImg1, CarouselImg2, CarouselImg1];
+  const images = [doctor1, doctor2, doctor3, doctor4, doctor5];
 
   return (
-    <Box>
+    <Box sx={{background:"#FFFFFF",  paddingTop: { xs: 2, sm: 4, lg: 6 },}}>
       <Typography
         variant="h3"
-        sx={{ fontSize: "26px", fontWeight: "600", color: "#1B3C74", marginBottom: 2 }}
+        sx={{
+          fontSize: "26px",
+          fontWeight: "600",
+          color: "#1B3C74",
+          marginBottom: 2,
+        }}
       >
         Our Medical Specialist
       </Typography>
@@ -25,29 +33,40 @@ const MedicalSpecialistCarousel = () => {
         navigation
         pagination={{ clickable: true }}
         autoplay={{ delay: 3000 }}
-        slidesPerView={3}
+        slidesPerView={4}
         breakpoints={{
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20, 
+              },
           640: {
-            slidesPerView: 1,
+            slidesPerView: 2,
             spaceBetween: 10,
           },
           768: {
-            slidesPerView: 2,
+            slidesPerView: 3,
             spaceBetween: 15,
           },
           1024: {
-            slidesPerView: 3,
+            slidesPerView: 4,
             spaceBetween: 20,
           },
         }}
       >
         {images.map((src, index) => (
           <SwiperSlide key={index}>
+            <Box 
+             sx={{background: "linear-gradient(144.2deg, #E1F3FF 0%, #2AA7FF 100%)",
+                paddingTop: "40px", 
+                borderTopLeftRadius: "50%",
+                borderTopRightRadius: "50%",
+             }}>
             <img
               src={src}
               alt={`Slide ${index + 1}`}
-              style={{ width: "100%", height: "auto", borderRadius: "10px" }}
+              style={{ width: "250px", height: "230px", borderRadius: "10px" }}
             />
+            </Box>
           </SwiperSlide>
         ))}
       </Swiper>
