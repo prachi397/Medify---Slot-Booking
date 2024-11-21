c9bb0e1bdaee218ae55f3b926dca654b43d5d4af
unknown <prachipanwar397@gmail.com>
2024-11-21 10:19:30 +0530

    added routers and stylings for navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import HomePage from './components/Home/HomePage';
+import FindDoctors from "./components/FindDoctors/FindDoctors";
+import MyBookings from "./components/MyBookings/MyBooking";
 
 function App() {
   return (
     <div className="App">
-     <HomePage/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/find-doctors" element={<FindDoctors/>}/>
+        <Route path="/my-bookings" element={<MyBookings/>}/>
+      </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./navbar.css";
 import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material";
 import MedifyLogo from "../../Assets/MedifyLogo.png";
+import { NavLink } from "react-router-dom";
 
-const Navbar = ({pageName}) => {
+const Navbar = ({ pageName }) => {
   return (
     <div className="navbar">
       <div className="upper-heading">
@@ -14,34 +15,56 @@ const Navbar = ({pageName}) => {
           cleanliness.
         </span>
       </div>
-      <AppBar position="static" className={pageName==="HomePage" ? "lower-navbar":'other-navbar'}>
-        <Toolbar sx={{
+      <AppBar
+        position="static"
+        className={pageName === "HomePage" ? "lower-navbar" : "other-navbar"}
+      >
+        <Toolbar
+          sx={{
             display: "flex",
             gap: "35px",
             justifyContent: "center",
-            alignItems: { xs: "flex-start", md: "center" }, 
-            flexWrap: { xs: "wrap", md: "nowrap" }, 
+            alignItems: { xs: "flex-start", md: "center" },
+            flexWrap: { xs: "wrap", md: "nowrap" },
             px: 2,
-        }}>
+          }}
+        >
           <IconButton aria-label="menu" sx={{ mr: 2 }}>
             <img src={MedifyLogo} alt="logo" width={80} height={27} />
           </IconButton>
-          <Box sx={{
-            display: "flex",
-            flexWrap: "wrap", 
-            gap: 2,
-            alignItems: "center",
-            mt: { xs: 2, md: 0 },
-          }}>
-          <Button color="inherit">Find Doctors</Button>
-          <Button color="inherit">Hospitals</Button>
-          <Button color="inherit">Medicines</Button>
-          <Button color="inherit">Surgeries</Button>
-          <Button color="inherit">Software for Provider</Button>
-          <Button color="inherit">Facilities</Button>
-          <Button className="booking-btn" variant="contained" color="primary">
-            My Bookings
-          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: 2,
+              alignItems: "center",
+              mt: { xs: 2, md: 0 },
+            }}
+          >
+            <NavLink
+              to="/find-doctors"
+              style={({ isActive }) => ({
+                color: isActive ? "#2AA7FF" : "inherit",
+                borderBottom: isActive ? "2px solid #2AA7FF" : "none",
+              })}
+            >
+              {" "}
+              <Button color="inherit">Find Doctors</Button>
+            </NavLink>
+            <Button color="inherit">Hospitals</Button>
+            <Button color="inherit">Medicines</Button>
+            <Button color="inherit">Surgeries</Button>
+            <Button color="inherit">Software for Provider</Button>
+            <Button color="inherit">Facilities</Button>
+            <Button
+              component={NavLink}
+              to="/my-bookings"
+              className="booking-btn"
+              variant="contained"
+              color="primary"
+            >
+              My Bookings
+            </Button>
           </Box>
         </Toolbar>
       </AppBar>
